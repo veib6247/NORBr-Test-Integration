b98baf5bc9f638d75a7d4fdc06d657f24e20bc6c
Bryan Olandres <ozoneblacklight@outlook.com>
2024-04-21 14:02:52 +0800

    minor styles and transitions

--- components/appNotification.vue
+++ components/appNotification.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="rounded-lg border border-blue-300/10 bg-blue-100 p-4">
-    <p class="text-sm text-blue-900">
+    <div class="text-sm text-blue-900">
       <slot></slot>
-    </p>
+    </div>
   </div>
 </template>
---
+++ components/appSkeletonLoader.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="flex flex-col gap-1">
+    <div class="h-4 w-full animate-pulse rounded-lg bg-gray-300"></div>
+    <div class="h-4 w-full animate-pulse rounded-lg bg-gray-300"></div>
+    <div class="h-4 w-full animate-pulse rounded-lg bg-gray-300"></div>
+    <div class="text-xs italic">
+      *This is a custom loading component, not an included behaviour by the
+      payment form. The merchant/integrator can customize this to their
+      preference.
+    </div>
+  </div>
+</template>
+
+<script lang="ts" setup></script>
+
+<style></style>
