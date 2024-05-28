5f8209819439eeb7e84205c1a78fbf1eff4d7c31
Bryan Olandres <ozoneblacklight@outlook.com>
2024-05-28 14:17:02 +0800

    spacing

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  css: ["~/assets/css/main.css"],
+  css: ['~/assets/css/main.css'],
   postcss: {
     plugins: {
       tailwindcss: {},
@@ -12,16 +12,16 @@ export default defineNuxtConfig({
     head: {
       link: [
         {
-          rel: "stylesheet",
-          href: "https://secure-assets-sandbox.norbr.io/stylesheet/1.9/norbr-client.min.css",
+          rel: 'stylesheet',
+          href: 'https://secure-assets-sandbox.norbr.io/stylesheet/1.9/norbr-client.min.css',
         },
       ],
 
       script: [
         {
-          src: "https://secure-assets-sandbox.norbr.io/javascript/1.9/norbr-client.min.js",
+          src: 'https://secure-assets-sandbox.norbr.io/javascript/1.9/norbr-client.min.js',
         },
       ],
     },
   },
-});
+})
